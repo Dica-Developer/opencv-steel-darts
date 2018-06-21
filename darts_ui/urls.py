d299cea05424617f218dd868364a9daa393c1eff
Will Benica <w.benica@gmail.com>
2018-06-21 09:40:51 +0200

    Add url patterns for games

--- darts_ui/urls.py
+++ darts_ui/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new_game/', views.new_game, name='new_game'),
+    path('game/', views.game, name='list_of_games'),
+    path('game/new_game/', views.new_game, name='new_game'),
+    path('game/<int:game_id>/', views.game, name='game_detail')
 ]
