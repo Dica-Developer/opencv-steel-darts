e194b2242b2d0c86dd944f0335aa9166eed26c9f
Will Benica <w.benica@gmail.com>
2018-06-21 04:37:16 +0200

    Add new_game to the list of known urls

--- orca_darts/urls.py
+++ orca_darts/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('darts_ui/', include('darts_ui.urls')),
+    path('', include('darts_ui.urls')),
+    # path('new_game', include('darts_ui.urls')),
+    # path('darts_ui/', include('darts_ui.urls')),
     path('admin/', admin.site.urls),
 ]
