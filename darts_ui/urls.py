d50662a68819b43d027cce1f3952b6dd8b14d325
Will Benica <w.benica@gmail.com>
2018-06-21 14:39:57 +0200

    Add urls to the urls list

--- darts_ui/urls.py
+++ darts_ui/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('game/', views.game, name='list_of_games'),
     path('game/new_game/', views.new_game, name='new_game'),
-    path('game/<int:game_id>/', views.game, name='game_detail')
+    path('game/<int:game_id>/', views.game_id, name='game_detail')
 ]
