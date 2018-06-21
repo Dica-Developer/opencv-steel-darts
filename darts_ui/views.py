e3101783d984343c1458d320d67bbe746ce5ef8e
Will Benica <w.benica@gmail.com>
2018-06-21 05:22:15 +0200

    Index view shouldn't fail if DB is empty

--- darts_ui/views.py
+++ darts_ui/views.py
@@ -11,24 +11,23 @@ from datetime import datetime
 
 @csrf_exempt
 def index(request):
-    # print("Request: {}".format(request))
+    # Try to get the last saved object for the dart recognition (if available), otherwise create one.
     status = DartsRecognitionStatus.objects.last()
+    print("Status exists:\n{}".format(status))
+    # If there isn't a status in the DB, create an initial one
+    if status is None:
+        initial_recognition = DartsRecognitionStatus(is_running=False, since=datetime.now())
+        initial_recognition.save()
+        status = DartsRecognitionStatus.objects.last()
     if request.method == 'POST':
-        try:
-            if return_status():
-                new_status = DartsRecognitionStatus(is_running=(not status.current_status()), since=datetime.now())
-                new_status.save()
-                return render(request,
-                              'darts_ui/index.html',
-                              {"status": new_status.is_running, "since": new_status.since}
-                              )
-        except Exception:
-            none_status = DartsRecognitionStatus(is_running=False, since=datetime.now())
-            none_status.save()
+        # try:
+        if return_status():
+            new_status = DartsRecognitionStatus(is_running=(not status.current_status()), since=datetime.now())
+            new_status.save()
             return render(request,
                           'darts_ui/index.html',
-                          {"status": none_status.is_running, "since": none_status.since})
-
+                          {"status": new_status.is_running, "since": new_status.since}
+                          )
     if status.current_status():
         return render(request, 'darts_ui/index.html', {"status": True, "since": status.since})
     else:
