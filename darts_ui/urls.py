3751f02330d20c23dfb7852e8f1b062734b0a2c1
Will Benica <w.benica@gmail.com>
2018-06-23 08:17:48 +0200

    Add an `app_name` to generalize urls

--- darts_ui/urls.py
+++ darts_ui/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'darts_ui'
 urlpatterns = [
     path('', views.index, name='index'),
     path('game/', views.game, name='list_of_games'),
